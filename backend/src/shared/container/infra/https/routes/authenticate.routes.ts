1c3c6fef4a6425d96f62d22ca676fa3d691ba7fa
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 19:56:32 -0300

    user auth router created

---
+++ backend/src/shared/container/infra/https/routes/authenticate.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "../../../../../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
+const authenticateRouter = Router();
+
+const authenticateUserController = new AuthenticateUserController();
+
+authenticateRouter.post("/sessions", authenticateUserController.handle);
+
+export { authenticateRouter };
