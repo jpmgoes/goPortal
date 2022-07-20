cfaa172830c2dc94feea55cdf7c1c81867915f77
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 19:57:12 -0300

    user and user auth routers was added to application

--- backend/src/shared/container/infra/https/routes/index.ts
+++ backend/src/shared/container/infra/https/routes/index.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { authenticateRouter } from "./authenticate.routes";
+import { usersRouter } from "./users.routes";
 
 const router = Router();
+
+router.use("/users", usersRouter);
+router.use(authenticateRouter);
 
 export { router };
