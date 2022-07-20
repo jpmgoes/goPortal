14f5ce4ca2f7e3f092ebbb1e7446f3e1bc3d398b
João Pedro de Moura Góes <jpmourag@LAPTOP-TJHADC92.localdomain>
2022-07-20 17:24:54 -0300

    set error for all app

--- backend/src/shared/container/infra/https/server.ts
+++ backend/src/shared/container/infra/https/server.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import * as bodyParser from "body-parser";
 import "reflect-metadata";
 import cors from "cors";
 
 import "../typeorm/database";
 import "../../../container";
+
+import { AppError } from "../../errors/AppError";
 
 const app = express();
 
@@ -15,6 +17,17 @@ app.use(express.json());
 
 //ex: http://localhost:3333/tmp/avatar/1234312-img.png
 app.use("/tmp", express.static("tmp"))
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof AppError) {
+		return res.status(err.statusCode).json({ message: err.message });
+	}
+
+	return res.status(500).json({
+		status: "error",
+		message: `Internal server error - ${err.message}`,
+	});
+});
 
 app.listen(3333, () => {
 	console.log("Server is running on port 3333");
