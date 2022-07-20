5425b7024dc464339af5f15f4dbc433910d10e6b
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 19:17:54 -0300

    auth global properties to auth added

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,1 +1,7 @@
-export default {};
+export default {
+	secret_token: "8b18ca909823cb665a3ef6e05e6a8fe7",
+	expires_in_token: "30s",
+	secret_refresh_token: "94a08da1fecbb6e8b46990538c7b50b2",
+	expires_in_refresh_token: "30d",
+	expires_refresh_token_days: 30,
+};
