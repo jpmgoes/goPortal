8d8670644733c8a88fdd92a405c4d416ed92b0e8
João Pedro de Moura Góes <jpmourag@LAPTOP-TJHADC92.localdomain>
2022-07-20 17:17:31 -0300

    set public static img route

--- backend/src/shared/container/infra/https/server.ts
+++ backend/src/shared/container/infra/https/server.ts
@@ -13,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
+//ex: http://localhost:3333/tmp/avatar/1234312-img.png
+app.use("/tmp", express.static("tmp"))
+
 app.listen(3333, () => {
 	console.log("Server is running on port 3333");
 });
