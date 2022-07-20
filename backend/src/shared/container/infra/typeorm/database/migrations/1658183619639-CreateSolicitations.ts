df3978f98057671d632439ece36832b26ba08bea
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 17:52:46 -0300

    solicitations migration created

---
+++ backend/src/shared/container/infra/typeorm/database/migrations/1658183619639-CreateSolicitations.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSolicitations1658183619639 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: "solicitations",
+				columns: [
+					{
+						name: "id",
+						type: "uuid",
+						isPrimary: true,
+					},
+					{
+						name: "name",
+						type: "varchar",
+					},
+					{
+						name: "description",
+						type: "varchar",
+					},
+					{
+						name: "is_open",
+						type: "boolean",
+						default: false
+					},
+					{
+						name: "user_id",
+						type: "varchar",
+					},
+					{
+						name: "create_at",
+						type: "timestamp",
+						default: "now()",
+					},
+				],
+			})
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable("solicitations");
+	}
+}
