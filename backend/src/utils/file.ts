f6b24b52ca331567b65545641fac117ed5944cc4
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 20:27:05 -0300

    delete files function was created

---
+++ backend/src/utils/file.ts
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+export const deleteFile = async (filename: string) => {
+	try {
+		await fs.promises.stat(filename);
+	} catch (error) {
+		return;
+	}
+	await fs.promises.unlink(filename);
+};
