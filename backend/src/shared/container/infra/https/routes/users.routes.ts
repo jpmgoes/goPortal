ada12c2869523905d39463af0f5a6843f7ab7402
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 19:56:11 -0300

    user router created

---
+++ backend/src/shared/container/infra/https/routes/users.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { CreateUserController } from "../../../../../modules/accounts/useCases/createUser/CreateUserController";
+
+const usersRouter = Router();
+
+const createUserController = new CreateUserController();
+usersRouter.post("/", createUserController.handle);
+
+export { usersRouter };
