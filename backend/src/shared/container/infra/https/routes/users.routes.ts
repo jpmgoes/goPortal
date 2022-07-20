9f5c366d9bb93d7dcd2071685c0b8e76e028a4bb
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 20:34:26 -0300

    upload or update avatar router added

--- backend/src/shared/container/infra/https/routes/users.routes.ts
+++ backend/src/shared/container/infra/https/routes/users.routes.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../../../../../config/upload";
 import { CreateUserController } from "../../../../../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../../../../../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRouter = Router();
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
+
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
 usersRouter.post("/", createUserController.handle);
 
+usersRouter.use(ensureAuthenticated);
+usersRouter.patch(
+	"/avatar",
+	uploadAvatar.single("avatar"),
+	updateUserAvatarController.handle
+);
 export { usersRouter };
