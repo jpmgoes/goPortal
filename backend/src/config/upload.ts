b85117d20adf0bc2b1ae593dbd4363201356ef76
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 19:19:42 -0300

    global upload file function added

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,18 +1,1 @@
-import multer from "multer";
-import { resolve } from "path";
-import * as crypto from "crypto";
-
-export default {
-	upload(folder: string = "tmp") {
-		return {
-			storage: multer.diskStorage({
-				destination: resolve(__dirname, "..", "..", folder),
-				filename: (req, file, cb) => {
-					const fileHash = crypto.randomBytes(16).toString("hex");
-					const fileName = `${fileHash}-${file.originalname}`;
-					return cb(null, fileName);
-				},
-			}),
-		};
-	},
-};
+export default {};
