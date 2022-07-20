0f7f05fddf105260d6c84365bd10bbc866d4dda3
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 18:00:18 -0300

    user entity created

---
+++ backend/src/modules/accounts/infra/entities/User.ts
@@ -0,0 +1,43 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+	@PrimaryColumn()
+	id: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	password: string;
+
+	@Column()
+	cpf: string;
+
+	@Column()
+	fone_number: string;
+
+	@Column()
+	profession: string;
+
+	@Column()
+	salary: string;
+
+	@Column()
+	avatar: string;
+
+	@CreateDateColumn()
+	created_at: Date;
+
+	constructor() {
+		if (!this.id) {
+			this.id = uuid();
+		}
+	}
+}
+
+export { User };
