1fafe56fbcf5a505d7d1155d3151b13cc589e6b6
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 20:19:50 -0300

    mail provider singleton that stars beforer everything in application ('registerInstance') was created

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,5 +1,11 @@
 import { container } from "tsyringe";
 import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DateProvider } from "./DateProvider/implementarions/DateProvider";
+import { MailtrapMailProvider } from "./Mail/implementations/MailtrapMailProvider";
 
 container.registerSingleton<IDateProvider>("DayjsDateProvider", DateProvider);
+
+container.registerInstance<IMailProvider>(
+	"MailtrapMailProvider",
+	new MailtrapMailProvider()
+);
