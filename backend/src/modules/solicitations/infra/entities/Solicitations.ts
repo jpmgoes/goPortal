eca0eb72a14ac1b9bf3b9f8b5ed69fe91c14d14a
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 18:07:28 -0300

    solicitations entity created

---
+++ backend/src/modules/solicitations/infra/entities/Solicitations.ts
@@ -0,0 +1,36 @@
+import { v4 as uuid } from "uuid";
+import {
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryColumn,
+} from "typeorm";
+
+@Entity("specifications")
+class Solicitations {
+	@PrimaryColumn()
+	public id?: string;
+
+	@Column()
+	public name: string;
+
+	@Column()
+	public description: string;
+
+	@Column()
+	public is_open: boolean;
+
+	@Column()
+	public user_id: string;
+
+	@CreateDateColumn()
+	public created_at?: Date;
+
+	constructor() {
+		if (!this.id) {
+			this.id = uuid();
+		}
+	}
+}
+
+export { Solicitations };
