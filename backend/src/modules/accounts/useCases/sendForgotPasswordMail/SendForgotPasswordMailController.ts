67252d86d77aec039f760c806eafc441cb48c2bf
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 20:25:33 -0300

    send forgot pass mail controller was created

---
+++ backend/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
+
+class SendForgotPasswordMailController {
+	async handle(req: Request, res: Response): Promise<Response> {
+		const { email } = req.body;
+		const sendForgotPasswordMailUseCase = container.resolve(
+			SendForgotPasswordMailUseCase
+		);
+
+		await sendForgotPasswordMailUseCase.execute(email);
+
+		return res.sendStatus(201);
+	}
+}
+export { SendForgotPasswordMailController };
