193852901febc066e12309aad51e6d5172f15eb5
jpmourag <jpmourag@LAPTOP-TJHADC92.localdomain>
2022-07-20 17:36:02 -0300

    set router

--- backend/src/shared/container/infra/https/server.ts
+++ backend/src/shared/container/infra/https/server.ts
@@ -7,6 +7,7 @@ import "../typeorm/database";
 import "../../../container";
 
 import { AppError } from "../../errors/AppError";
+import { router } from "./routes";
 
 const app = express();
 
@@ -17,6 +18,8 @@ app.use(express.json());
 
 //ex: http://localhost:3333/tmp/avatar/1234312-img.png
 app.use("/tmp", express.static("tmp"))
+
+app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	if (err instanceof AppError) {
