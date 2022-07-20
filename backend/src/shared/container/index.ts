8875a63c430d463979e9376c75ef170b31db0693
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 20:00:07 -0300

    users token singleton created

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
 import { UsersRepository } from "../../modules/accounts/infra/repositories/UsersRepository";
+import { UsersTokensRepository } from "../../modules/accounts/infra/repositories/UsersTokensRepository";
 import { IUsersRepository } from "../../modules/accounts/repositories/IUserRepository";
+import { IUsersTokensRepository } from "../../modules/accounts/repositories/IUserTokensRepository";
 
 container.registerSingleton<IUsersRepository>(
 	"UsersRepository",
 	UsersRepository
+);
+
+container.registerSingleton<IUsersTokensRepository>(
+	"UsersTokensRepository",
+	UsersTokensRepository
 );
