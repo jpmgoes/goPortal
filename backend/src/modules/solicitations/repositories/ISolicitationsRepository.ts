5e3e340533a5f2397f65b722d6ad3be09d77effd
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 20:59:36 -0300

    findById added to interface and Id was added to create parameter

--- backend/src/modules/solicitations/repositories/ISolicitationsRepository.ts
+++ backend/src/modules/solicitations/repositories/ISolicitationsRepository.ts
@@ -2,12 +2,14 @@ import { Solicitations } from "../infra/entities/Solicitations";
 
 interface ISolicitationsRepository {
 	create({
+		id,
 		name,
 		description,
 		user_id,
 		is_open,
 	}: ICreateSolicitationsDTO): Promise<Solicitations>;
 	findByName(name: string): Promise<Solicitations>;
+	findById(id: string): Promise<Solicitations>;
 	list(user_id: string): Promise<Solicitations[]>;
 }
 
