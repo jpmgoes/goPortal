0dcc5878e0979dd52e9de630c3361a5df06f03d4
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 18:08:48 -0300

    migrations added to db

--- backend/src/shared/container/infra/typeorm/database/index.ts
+++ backend/src/shared/container/infra/typeorm/database/index.ts
@@ -1,6 +1,7 @@
 import { DataSource } from "typeorm";
 import { CreateSolicitations1658183619639 } from "./migrations/1658183619639-CreateSolicitations";
 import { CreateUsers1658187014088 } from "./migrations/1658187014088-CreateUsers";
+import { CreateUsersTokens1658273954012 } from "./migrations/1658273954012-CreateUsersTokens";
 
 export const AppDataSource = new DataSource({
 	type: "better-sqlite3",
@@ -9,7 +10,11 @@ export const AppDataSource = new DataSource({
 	logging: true,
 	entities: [],
 	subscribers: [],
-	migrations: [CreateUsers1658187014088, CreateSolicitations1658183619639],
+	migrations: [
+		CreateUsers1658187014088,
+		CreateSolicitations1658183619639,
+		CreateUsersTokens1658273954012,
+	],
 });
 
 AppDataSource.initialize()
