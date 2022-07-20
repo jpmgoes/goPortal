ade3641d0b80ef9598ebce68fb15396fdcd7b3f0
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 20:18:00 -0300

    mail provider class using mailtrap provider created

---
+++ backend/src/shared/container/providers/Mail/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,39 @@
+import { injectable } from "tsyringe";
+import nodemailer, { Transporter } from "nodemailer";
+import * as fs from "fs";
+import Handlebars from "handlebars";
+@injectable()
+class MailtrapMailProvider implements IMailProvider {
+	private client: Transporter;
+	constructor() {
+		const transport = nodemailer.createTransport({
+			host: "smtp.mailtrap.io",
+			port: 2525,
+			auth: {
+				user: "293c13ed759d63",
+				pass: "feab579078cac3",
+			},
+		});
+		this.client = transport;
+	}
+
+	async sendMail(
+		to: string,
+		subject: string,
+		variables: any,
+		path: string
+	): Promise<void> {
+		const templateFileContent = fs.readFileSync(path).toString("utf-8");
+		const templateParse = Handlebars.compile(templateFileContent);
+		const templateHtml = templateParse(variables);
+
+		await this.client.sendMail({
+			from: "Portal <noreply@portal.com.br>",
+			to,
+			subject,
+			html: templateHtml,
+		});
+	}
+}
+
+export { MailtrapMailProvider };
