078b259f90e89dfc05629a3902397e691ed4096d
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 21:47:59 -0300

    get link from client to send the correct link to user email

--- backend/src/modules/solicitations/useCases/closeSolicitation/CloseSolicitationController.ts
+++ backend/src/modules/solicitations/useCases/closeSolicitation/CloseSolicitationController.ts
@@ -4,11 +4,13 @@ import { CloseSolicitationUseCases } from "./CloseSolicitationUseCases";
 
 class CloseSolicitationController {
 	async handle(req: Request, res: Response): Promise<Response> {
+		const { link } = req.body;
 		const { id } = req.body;
+
 		const createCateogryUseCase = container.resolve(
 			CloseSolicitationUseCases
 		);
-		await createCateogryUseCase.execute(id);
+		await createCateogryUseCase.execute({ id, link });
 		return res.send();
 	}
 }
