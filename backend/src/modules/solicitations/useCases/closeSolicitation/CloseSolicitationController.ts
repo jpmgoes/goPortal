dfcc8288e01e4d3741ce1b2cc0d2e8d905af467c
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 23:59:41 -0300

    id moved

--- backend/src/modules/solicitations/useCases/closeSolicitation/CloseSolicitationController.ts
+++ backend/src/modules/solicitations/useCases/closeSolicitation/CloseSolicitationController.ts
@@ -4,8 +4,7 @@ import { CloseSolicitationUseCases } from "./CloseSolicitationUseCases";
 
 class CloseSolicitationController {
 	async handle(req: Request, res: Response): Promise<Response> {
-		const { link, reply } = req.body;
-		const { id } = req.body;
+		const { id, link, reply } = req.body;
 
 		const createCateogryUseCase = container.resolve(
 			CloseSolicitationUseCases
