adf2881fcdf7a228e57f598add2bc74cbba345f8
jpmourag <jp.mouragoes@gmail.com>
2022-07-21 20:18:04 -0300

    sharing global context for all app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { AppContext } from "./context/AppContext";
+import AppRoutes from "./routes";
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<AppContext>
+			<BrowserRouter>
+				<AppRoutes />
+			</BrowserRouter>
+		</AppContext>
+	);
 }
 
 export default App;
