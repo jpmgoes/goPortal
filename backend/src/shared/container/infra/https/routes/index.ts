c3f1a8420b4fc0b0e2e5b373f44c7ef4cd15ffef
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 22:17:49 -0300

    adding passwordRouter

--- backend/src/shared/container/infra/https/routes/index.ts
+++ backend/src/shared/container/infra/https/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authenticateRouter } from "./authenticate.routes";
+import { passwordRouter } from "./password.routes";
 import { usersRouter } from "./users.routes";
 
 const router = Router();
 
 router.use("/users", usersRouter);
 router.use(authenticateRouter);
+router.use("/password", passwordRouter)
 
 export { router };
