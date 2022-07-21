60c3a86e95503a8dc82e28f8b260b64b4b327ced
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 21:02:10 -0300

    close solicitations controller was created

---
+++ backend/src/modules/solicitations/useCases/closeSolicitation/CloseSolicitationController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CloseSolicitationUseCases } from "./CloseSolicitationUseCases";
+
+class CloseSolicitationController {
+	async handle(req: Request, res: Response): Promise<Response> {
+		const { id } = req.body;
+		const createCateogryUseCase = container.resolve(
+			CloseSolicitationUseCases
+		);
+		await createCateogryUseCase.execute(id);
+		return res.sendStatus(201);
+	}
+}
+
+export { CloseSolicitationController };
