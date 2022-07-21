fbbc1bb7bc889bf68a1330798f0f9b2d8a9ccb2c
jpmourag <jp.mouragoes@gmail.com>
2022-07-21 01:21:57 -0300

    update mail routes created

--- backend/src/shared/container/infra/https/routes/users.routes.ts
+++ backend/src/shared/container/infra/https/routes/users.routes.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import multer from "multer";
 import uploadConfig from "../../../../../config/upload";
 import { CreateUserController } from "../../../../../modules/accounts/useCases/createUser/CreateUserController";
-import { UpdateUserControler } from "../../../../../modules/accounts/useCases/updateUser/updateUserUseCaseController";
+import { ResetMailUserController } from "../../../../../modules/accounts/useCases/resetMailUser/ResetMailUserController";
+import { SendChangeUserMailController } from "../../../../../modules/accounts/useCases/sendChangeUserMail/SendChangeUserMailController";
+import { UpdateUserController } from "../../../../../modules/accounts/useCases/updateUser/UpdateUserController";
 import { UpdateUserAvatarController } from "../../../../../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -12,7 +14,10 @@ const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
-const updateUserControler = new UpdateUserControler();
+const updateUserControler = new UpdateUserController();
+const sendChangeUserMailController = new SendChangeUserMailController();
+const resetMailUserController = new ResetMailUserController();
+
 usersRouter.post("/", createUserController.handle);
 
 usersRouter.use(ensureAuthenticated);
@@ -22,4 +27,8 @@ usersRouter.patch(
 	updateUserAvatarController.handle
 );
 usersRouter.post("/update", updateUserControler.handle);
+
+usersRouter.post("/update/email", sendChangeUserMailController.handle);
+usersRouter.post("/update/email/reset", sendChangeUserMailController.handle);
+
 export { usersRouter };
