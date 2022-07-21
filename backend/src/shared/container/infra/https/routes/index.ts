0f6f303e79fb3f44af71b0a4ab3819fe3034a453
jpmourag <jp.mouragoes@gmail.com>
2022-07-20 23:50:40 -0300

    type fixed

--- backend/src/shared/container/infra/https/routes/index.ts
+++ backend/src/shared/container/infra/https/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authenticateRouter } from "./authenticate.routes";
 import { passwordRouter } from "./password.routes";
+import { solicitationsRouter } from "./solicitations.routes";
 import { usersRouter } from "./users.routes";
 
 const router = Router();
@@ -8,6 +9,6 @@ const router = Router();
 router.use("/users", usersRouter);
 router.use(authenticateRouter);
 router.use("/password", passwordRouter)
-router.use("/solicitation", passwordRouter)
+router.use("/solicitation", solicitationsRouter)
 
 export { router };
