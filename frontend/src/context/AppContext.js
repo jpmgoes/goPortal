17734b26b6f771199e70ead07373f7b9b473617e
jpmourag <jp.mouragoes@gmail.com>
2022-07-21 21:40:19 -0300

    adding userContext

--- frontend/src/context/AppContext.js
+++ frontend/src/context/AppContext.js
@@ -1,15 +1,19 @@
 import React, { createContext } from "react";
 import useAuth from "./hooks/useAuth";
 import useSolicitation from "./hooks/useSolicitation";
+import useUser from "./hooks/useUser";
 
 const Context = createContext();
 
 const AppContext = ({ children }) => {
 	const authContext = useAuth();
 	const soliticationContext = useSolicitation();
+	const userContext = useUser();
 
 	return (
-		<Context.Provider value={{ authContext, soliticationContext }}>
+		<Context.Provider
+			value={{ authContext, soliticationContext, userContext }}
+		>
 			{children}
 		</Context.Provider>
 	);
