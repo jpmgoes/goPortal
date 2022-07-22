3aac224de0e86940de15b238772b7a2d1669c9c0
jpmourag <jp.mouragoes@gmail.com>
2022-07-22 12:04:32 -0300

    css styles added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import "./styles/index.css";
+import "./styles/form.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
