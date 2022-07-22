13d4c9c7cc6b0f6269c3f6924a155f27f86bc4c6
jpmourag <jp.mouragoes@gmail.com>
2022-07-22 02:30:08 -0300

    routers created

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,28 +2,24 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-
-// import { Context } from './Context/AuthContext';
-
-function CustomRoute({ isPrivate, ...rest }) {
-	//   const { loading, authenticated } = useContext(Context);
-
-	//   if (loading) {
-	//     return <h1>Loading...</h1>;
-	//   }
-
-	//   if (isPrivate && !authenticated) {
-	//     return <Redirect to="/login" />
-	//   }
-
-	return <Route {...rest} />;
-}
+import { Loading } from "./pages/Loading";
+import { UserAccount } from "./pages/UserAccount";
+import { ChangePassword } from "./pages/ChangePassword";
+import { ChangeEmail } from "./pages/ChangeEmail";
+import { Solicitation } from "./pages/Solicitation";
+import { Register } from "./pages/Register";
 
 export default function AppRoutes() {
 	return (
 		<Routes>
-			{CustomRoute({ exact: true, path: "/login", element: <Login /> })}
-			{CustomRoute({ exact: true, path: "/", element: <Home /> })}
+			<Route exact path="/" element={<Home />} />
+			<Route exact path="/login" element={<Login />} />
+			<Route exact path="/loading" element={<Loading />} />
+			<Route exact path="/register" element={<Register />} />
+			<Route exact path="/account" element={<UserAccount />} />
+			<Route exact path="/change/pass" element={<ChangePassword />} />
+			<Route exact path="/change/email" element={<ChangeEmail />} />
+			<Route exact path="/solicitation" element={<Solicitation />} />
 		</Routes>
 	);
 }
