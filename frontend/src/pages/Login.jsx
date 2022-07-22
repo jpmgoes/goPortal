ec58323fd369042ad7685febbcfd9d613381e313
jpmourag <jp.mouragoes@gmail.com>
2022-07-22 12:00:23 -0300

    added a fn to request api and handle erros to show for user, and called a template for the page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../context/AppContext";
 
 import { useNavigate } from "react-router-dom";
+import { TemplateFormPages } from "../components/templates/TemplateFormPages";
+import { LoginForm } from "../components/forms/LoginForm";
 
 function Login() {
-	const context = useContext(Context);
+	const { authContext } = useContext(Context);
 	const navigate = useNavigate();
-	if (context["authContext"]) navigate("/");
+	const [error, setError] = useState(<></>);
 
-	return <div>Login</div>;
+	useEffect(() => {
+		authContext.setLoading("/");
+		if (authContext.authenticated) navigate("/loading");
+	}, [authContext, authContext.authenticated, navigate]);
+
+	const onSubmit = ({ email, password }) => {
+		authContext.handleLogin(email, password);
+		if (authContext.error)
+			if (authContext.error.response.status === 400) {
+				setError(<p className="error">*Email ou senha incorretos</p>);
+			}
+	};
+
+	return (
+		<TemplateFormPages title={"LOGIN"}>
+			<LoginForm onSubmit={onSubmit} error={error} />
+			<p onClick={() => navigate("/register")}>Criar uma conta</p>
+			<p onClick={() => navigate("/change/pass/request")}>
+				Esqueci minha senha
+			</p>
+		</TemplateFormPages>
+	);
 }
 
 export default Login;
