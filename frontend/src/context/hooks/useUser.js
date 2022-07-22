b757e15d624267f045703af97fb39a9f5adb351a
jpmourag <jp.mouragoes@gmail.com>
2022-07-21 21:38:30 -0300

    useUser renamed

--- frontend/src/context/hooks/useUser.js
+++ frontend/src/context/hooks/useUser.js
@@ -2,18 +2,23 @@ import { useState, useEffect } from "react";
 
 import api from "../../api";
 
-export default function useAuth() {
+export default function useUser() {
 	const [user, setUser] = useState(null);
 
 	useEffect(() => {
 		const token = localStorage.getItem("token");
-		
+
 		if (token) {
 			api.get("/users").then((res) => {
 				setUser(res.data.user);
 			});
 		}
 	}, []);
+
+	// [--] mudar senha
+	// [--] editar perfil
+	// [--] mudar avatar
+	// [--] solicitar alterar email
 
 	return { user };
 }
