23e443199766290f5b247fa847149c942e29d243
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:45:20 -0300

    useAuth was deleted

--- frontend/src/context/AppContext.js
+++ frontend/src/context/AppContext.js
@@ -1,19 +1,16 @@
 import React, { createContext } from "react";
-import useAuth from "./hooks/useAuth";
 import useSolicitation from "./hooks/useSolicitation";
 import useUser from "./hooks/useUser";
 
 const Context = createContext();
 
 const AppContext = ({ children }) => {
-	const authContext = useAuth();
 	const soliticationContext = useSolicitation();
 	const userContext = useUser();
 
 	return (
 		<Context.Provider
 			value={{
-				authContext,
 				soliticationContext,
 				userContext,
 			}}
