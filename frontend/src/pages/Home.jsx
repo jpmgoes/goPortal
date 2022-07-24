ae36a7b0d9d7746e286cb14b26f29828997edc13
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:05:20 -0300

    added table and the buttons to go to badge page and contra-cheque page home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,36 @@ import React, { useContext } from "react";
 import { Context } from "../context/AppContext";
 
 const Home = () => {
-	const context = useContext(Context);
-	console.log(context?.soliticationContext?.solicitations);
-	return <div>Home {"userArray"}</div>;
+	const navigate = useNavigate();
+	const { userContext } = useContext(Context);
+
+	useAuthHook("/");
+	if (userContext.user?.avatar === "http://localhost:3333/tmp/avatar/")
+		return <UploadImagePage />;
+	if (!userContext.user) return <Loading />;
+	if (!userContext.authenticated) return <></>;
+
+	return (
+		<MainTemplate>
+			<div className="home-content">
+				<div className="home-bts">
+					<button
+						className="baseTableLayout"
+						onClick={() => navigate("/badge")}
+					>
+						Ver meu crachá digital
+					</button>
+					<button
+						className="baseTableLayout"
+						onClick={() => navigate("/contra-cheque")}
+					>
+						Ver meu contra cheque
+					</button>
+				</div>
+				<Table />
+			</div>
+		</MainTemplate>
+	);
 };
 
 export default Home;
