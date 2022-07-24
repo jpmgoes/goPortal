c89af731f0c1a17b4877e9a6c24901c05d3e071c
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:23:36 -0300

    condicional to private some routes was created

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
@@ -8,22 +8,42 @@ import { ChangePassword } from "./pages/ChangePassword";
 import { Solicitation } from "./pages/Solicitation";
 import { Register } from "./pages/Register";
 import { RequestChangePass } from "./pages/RequestChangePass";
+import { Badge } from "./pages/Badge";
+import { ContraCheque } from "./pages/ContraCheque";
+import { Context } from "./context/AppContext";
 
 export default function AppRoutes() {
+	const { userContext } = useContext(Context);
 	return (
 		<Routes>
 			<Route exact path="/" element={<Home />} />
 			<Route exact path="/login" element={<Login />} />
 			<Route exact path="/loading" element={<Loading />} />
 			<Route exact path="/register" element={<Register />} />
-			<Route exact path="/account" element={<UserAccount />} />
 			<Route exact path="/change/pass" element={<ChangePassword />} />
 			<Route
 				exact
 				path="/change/pass/request"
 				element={<RequestChangePass />}
 			/>
-			<Route exact path="/solicitation" element={<Solicitation />} />
+			{userContext.authenticated ? (
+				<>
+					<Route exact path="/account" element={<UserAccount />} />
+					<Route
+						exact
+						path="/solicitation"
+						element={<Solicitation />}
+					/>
+					<Route exact path="/badge" element={<Badge />} />
+					<Route
+						exact
+						path="/contra-cheque"
+						element={<ContraCheque />}
+					/>
+				</>
+			) : (
+				<></>
+			)}
 		</Routes>
 	);
 }
