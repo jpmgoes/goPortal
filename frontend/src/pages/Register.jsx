84d009cc6da399f805791080beaf3bf268578eb1
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:59:14 -0300

    using useEffect to get a better way to handle response errors

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,25 +1,32 @@
-import React, { useContext } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { RegisterForm } from "../components/forms/RegisterForm";
 import { TemplateFormPages } from "../components/templates/TemplateFormPages";
 import { Context } from "../context/AppContext";
 
+import { toast } from "react-toastify";
+
 export const Register = () => {
 	const { userContext } = useContext(Context);
 	const navigate = useNavigate();
 
+	useEffect(() => {
+		if (userContext.response?.status === 201) {
+			toast("Conta criada com sucesso!");
+			navigate("/login");
+		} else if (userContext.response?.status === 400) {
+			if (userContext.response.data.message === "User Alredy Exist") {
+				toast("Email já cadastrado! Não foi possível criar uma conta!");
+			} else {
+				toast("Não foi possível criar a conta!");
+			}
+		}
+		userContext.setResponse(null);
+	}, [userContext.response]);
+
 	const onSubmit = (data) => {
 		userContext.createUser(data);
-		if (!userContext.error) {
-		} else if (userContext.error["status"] === 400) {
-			alert("Usuário já cadastrado");
-			navigate("/login");
-		} else if (userContext.error["status"] === 200) {
-			alert("Usuário cadastrado com sucesso");
-			navigate("/login");
-		} else {
-			alert("Erro ao cadastrar usuário");
-		}
 	};
 
 	return (
