35a33921d1e158761dbd5698fc64444012edecbf
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:10:52 -0300

    form erros moved around

--- frontend/src/components/forms/LoginForm.jsx
+++ frontend/src/components/forms/LoginForm.jsx
@@ -1,36 +1,33 @@
 import { useForm } from "react-hook-form";
 
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 import { ShowPass } from "../ShowPass";
 import { useState } from "react";
-
-const schema = yup
-	.object({
-		email: yup.string().required("Email é obrigatório"),
-		password: yup.string().required("Senha é obrigatória"),
-	})
-	.required();
 
 export const LoginForm = ({ onSubmit, error }) => {
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm({
-		resolver: yupResolver(schema),
-	});
+	} = useForm();
 
 	const [passShown, setPassShown] = useState("password");
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			{errors.email?.message}
-			<input placeholder="Email" {...register("email")} type="email" />
-			{errors.password?.message}
+			{errors.email && (
+				<span className="warn-span">Campo Obrigatório</span>
+			)}
+			<input
+				placeholder="Email"
+				{...register("email", { required: true })}
+				type="email"
+			/>
+			{errors.password && (
+				<span className="warn-span">Campo Obrigatório</span>
+			)}
 			<input
 				placeholder="Password"
-				{...register("password")}
+				{...register("password", { required: true })}
 				type={passShown}
 			/>
 			<ShowPass setPassShown={setPassShown} passShown={passShown} />
