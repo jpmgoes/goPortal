74cd58e78cd6ae7568e9e33fd4d821b0ebf02bc1
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:09:46 -0300

    form erros moved around

--- frontend/src/components/forms/ChangePassForm.jsx
+++ frontend/src/components/forms/ChangePassForm.jsx
@@ -1,6 +1,3 @@
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { ShowPass } from "../ShowPass";
@@ -10,24 +7,26 @@ export const ChangePassForm = ({ onSubmit, error }) => {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm({
-		resolver: yupResolver(schema),
-	});
+	} = useForm();
 
 	const [passShown, setPassShown] = useState("password");
 
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			{errors.password?.message}
+			{errors.password && (
+				<span className="warn-span">Campo Obrigatório</span>
+			)}
 			<input
 				placeholder="Nova Senha"
-				{...register("password")}
+				{...register("password", { required: true })}
 				type={passShown}
 			/>
-			{errors.confirm_password?.message}
+			{errors.confirm_password && (
+				<span className="warn-span">Campo Obrigatório</span>
+			)}
 			<input
 				placeholder="Confirmar Senha"
-				{...register("confirm_password")}
+				{...register("confirm_password", { required: true })}
 				type={passShown}
 			/>
 			<ShowPass setPassShown={setPassShown} passShown={passShown} />
