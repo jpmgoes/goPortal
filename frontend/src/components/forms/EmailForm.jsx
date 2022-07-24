1cc0b8434ca7354c2f1c5410916c04149369ea81
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:10:28 -0300

    form erros moved around

--- frontend/src/components/forms/EmailForm.jsx
+++ frontend/src/components/forms/EmailForm.jsx
@@ -1,31 +1,23 @@
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
 import { useForm } from "react-hook-form";
 
-const schema = yup
-	.object({
-		email: yup.string().required("Email é obrigatório"),
-	})
-	.required();
-export const EmailForm = ({ onSubmit, error }) => {
+export const EmailForm = ({ onSubmit }) => {
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm({
-		resolver: yupResolver(schema),
-	});
+	} = useForm();
+
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
-			{errors.email?.message}
+			{errors.email && (
+				<span className="warn-span">Campo Obrigatório</span>
+			)}
 			<input
 				placeholder="Email"
 				{...register("email", { required: true })}
 				type="email"
 			/>
 			<input className="button" type="submit" />
-			{error}
 		</form>
 	);
 };
