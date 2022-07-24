cc3558265c33505ee06ad2e775620437fcd41cfe
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:51:22 -0300

    change alert to notify with toast

--- frontend/src/pages/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword.jsx
@@ -1,28 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ChangePassForm } from "../components/forms/ChangePassForm";
 import { TemplateFormPages } from "../components/templates/TemplateFormPages";
 import { Context } from "../context/AppContext";
+import { toast } from "react-toastify";
 
 export const ChangePassword = () => {
+	const [error, setError] = useState("");
 	const { userContext } = useContext(Context);
 	const navigate = useNavigate();
 
-	const onSubmit = ({ password }) => {
-		userContext.handleNewPassword(password);
-		if (!userContext.error) {
-		} else if (userContext.error["status"] === 400) {
-		} else if (userContext.error["status"] === 200) {
-			alert("Senha alterada com sucesso");
+	useEffect(() => {
+		if (userContext.response?.status === 200) {
+			toast("Senha alterada!");
 			navigate("/login");
-		} else {
-			alert("Erro ao mudar senha");
+		} else if (userContext.response?.status === 400)
+			toast("Não foi possível alterar a senha!");
+		userContext.setResponse(null);
+	}, [userContext.response, userContext.responseAvatar]);
+
+	const onSubmit = ({ password, confirm_password }) => {
+		if (password !== confirm_password) {
+			setError(<div className="error">As senhas não são iguais</div>);
+			return;
 		}
+		userContext.handleNewPassword(password);
 	};
 
 	return (
 		<TemplateFormPages title={"MUDAR SENHA"}>
-			<ChangePassForm onSubmit={onSubmit}></ChangePassForm>
+			<ChangePassForm onSubmit={onSubmit} error={error}></ChangePassForm>
 		</TemplateFormPages>
 	);
 };
