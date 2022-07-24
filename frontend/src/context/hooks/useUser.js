1276599b49008a9472c11a73a83f3df959a4b656
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 19:48:07 -0300

    adding try catch to get the response and added functions from old useAuth

--- frontend/src/context/hooks/useUser.js
+++ frontend/src/context/hooks/useUser.js
@@ -5,49 +5,70 @@ import api from "../../api";
 export default function useUser() {
 	const [user, setUser] = useState(null);
 	const [userImg, setUserImg] = useState(null);
-	const [error, setError] = useState(null);
+	const [response, setResponse] = useState(null);
+	const [responseUser, setResponseUser] = useState(null);
+	const [responseAvatar, setResponseAvatar] = useState(null);
+	const [authenticated, setAuthenticated] = useState(false);
+	const [loading, setLoading] = useState("/");
 
 	useEffect(() => {
+		getUser();
+	}, []);
+
+	async function getUser() {
 		const token = localStorage.getItem("token");
 		api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
 		if (token) {
-			console.log("setUser");
-			api.get("/users").then((res) => {
-				setUser(res.data.user);
-			});
+			api.get("/users")
+				.then((res) => {
+					setUser(res.data.user);
+					setUserImg(res.data.user?.avatar);
+					setResponseUser(res);
+					if (
+						res.data.user?.avatar ===
+						"http://localhost:3333/tmp/avatar/"
+					)
+						setAuthenticated(false);
+					else setAuthenticated(true);
+				})
+				.catch((error) => {
+					setResponseUser(error.response);
+					setAuthenticated(false);
+				});
 		}
-	}, []);
+	}
 
 	async function createUser(data) {
 		try {
 			const res = await api.post("/users", data);
-			setError(res);
+			setResponse(res);
 		} catch (err) {
-			setError(err.response);
+			setResponse(err.response);
 		}
 	}
 
 	async function handleRequestNewPassword(email) {
 		const link = "http://localhost:3000/change/pass";
-		api.post("/password/forgot", { email, link })
-			.then((res) => {
-				setError(res);
-			})
-			.catch((err) => {
-				setError(err.response);
-			});
+		try {
+			const res = await api.post("/password/forgot", { email, link });
+			setResponse(res);
+		} catch (error) {
+			setResponse(error.response);
+		}
 	}
 
-	//! testar
 	async function handleNewPassword(password) {
 		const token = window.location.search.split("=")[1];
-		api.post("/password/reset", { password }, { params: { token } })
-			.then((res) => {
-				setError(res);
-			})
-			.catch((err) => {
-				setError(err.response);
-			});
+		try {
+			const res = await api.post(
+				"/password/reset",
+				{ password },
+				{ params: { token } }
+			);
+			setResponse(res);
+		} catch (error) {
+			setResponse(error.response);
+		}
 	}
 
 	async function handleUpdateUser(
@@ -57,33 +78,73 @@ export default function useUser() {
 		salary = "",
 		fone_number = ""
 	) {
-		console.log("handleUpdateUser");
-		await api.put("/users/update", {
-			name,
-			password,
-			profession,
-			salary,
-			fone_number,
-		});
+		try {
+			const res = await api.put("/users/update", {
+				name,
+				password,
+				profession,
+				salary,
+				fone_number,
+			});
+			setResponse(res);
+		} catch (error) {
+			setResponse(error.response);
+		}
 	}
 
 	async function handleUpdateAvatar() {
-		console.log("handleUpdateAvatar");
-
 		const formData = new FormData();
 		formData.append("avatar", userImg);
 
-		await api.patch("/users/avatar", formData, {
-			headers: {
-				"Content-Type": "multipart/form-data",
-			},
-		});
+		try {
+			const res = await api.patch("/users/avatar", formData, {
+				headers: {
+					"Content-Type": "multipart/form-data",
+				},
+			});
+			setResponseAvatar(res);
+		} catch (error) {
+			setResponseAvatar(error.response);
+		}
+	}
+
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+
+		if (token) {
+			api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+			setAuthenticated(true);
+		}
+	}, []);
+
+	async function handleLogin(email, password) {
+		try {
+			const res = await api.post("/sessions", { email, password });
+			localStorage.setItem(
+				"token",
+				JSON.stringify(res.data.refresh_token)
+			);
+			api.defaults.headers.Authorization = `Bearer ${res.data.refresh_token}`;
+			setAuthenticated(true);
+			setLoading("/");
+			setResponse(res);
+		} catch (error) {
+			setResponse(error.response);
+		}
+	}
+
+	function handleLogout() {
+		setAuthenticated(false);
+		localStorage.removeItem("token");
+		api.defaults.headers.Authorization = undefined;
+		setUser(null);
+		window.location.href = "http://localhost:3000/";
 	}
 
 	return {
 		user,
+		getUser,
 		createUser,
-		error,
 		setUser,
 		userImg,
 		setUserImg,
@@ -91,5 +152,16 @@ export default function useUser() {
 		handleRequestNewPassword,
 		handleUpdateUser,
 		handleUpdateAvatar,
+		response,
+		setResponse,
+		responseAvatar,
+		setResponseAvatar,
+		responseUser,
+		setResponseUser,
+		handleLogin,
+		handleLogout,
+		loading,
+		authenticated,
+		setLoading,
 	};
 }
