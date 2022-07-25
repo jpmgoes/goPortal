7be49f8756901cb354a3cd96586a84b99f08460e
jpmourag <jp.mouragoes@gmail.com>
2022-07-24 22:00:30 -0300

    get all solicitations router created

--- backend/src/shared/container/infra/https/routes/solicitations.routes.ts
+++ backend/src/shared/container/infra/https/routes/solicitations.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CloseSolicitationController } from "../../../../../modules/solicitations/useCases/closeSolicitation/CloseSolicitationController";
 import { CreateSolicitationController } from "../../../../../modules/solicitations/useCases/createSolicitation/CreateSolicitationController";
+import { GetAllSolicitationsController } from "../../../../../modules/solicitations/useCases/getAllSolicitations/GetAllSolicitationsController";
 import { ListSolicitationsController } from "../../../../../modules/solicitations/useCases/listSolicitations/ListSolicitationsController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -9,11 +10,13 @@ const solicitationsRouter = Router();
 const createSolicitationController = new CreateSolicitationController();
 const listSolicitationsController = new ListSolicitationsController();
 const closeSolicitationController = new CloseSolicitationController();
+const getAllSolicitationsController = new GetAllSolicitationsController();
 
 solicitationsRouter.use(ensureAuthenticated);
 
 solicitationsRouter.post("/", createSolicitationController.handle);
 solicitationsRouter.get("/", listSolicitationsController.handle);
 solicitationsRouter.post("/close", closeSolicitationController.handle);
+solicitationsRouter.get("/all", getAllSolicitationsController.handle);
 
 export { solicitationsRouter };
